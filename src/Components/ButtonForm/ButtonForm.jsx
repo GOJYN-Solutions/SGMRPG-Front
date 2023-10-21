9106fdb431b7afd7312d9316988a0c979688a775
Otaviodias21 <Otavio2011dias@hotmail.com>
2023-10-21 11:13:33 -0300

    Testando css responsivo com vh

--- src/Components/ButtonForm/ButtonForm.jsx
+++ src/Components/ButtonForm/ButtonForm.jsx
@@ -1,6 +1,6 @@
 export default function ButtonForm({onClick, text, className}){
 
-    let style = "w-60 h-16 text-xl rounded-3xl font-nonito"
+    let style = "w-[22.22vh] h-[5.92vh] text-[1.85vh] rounded-[2.22vh] font-nonito"
 
     return(
         <button className={`${className} ${style} `} onClick={onClick}>
--- src/Pages/ForgotPassword/ForgotPassword.jsx
+++ src/Pages/ForgotPassword/ForgotPassword.jsx
@@ -74,23 +74,23 @@ export default function Register() {
             case 'tel':
                 return(
                     <div className='h-full gap-10 flex flex-col items-center justify-center'>
-                        <p className='text-5xl font-nonito'>ESQUECEU A SENHA?</p>
-                        <p className='text-xl text-center'>Enviamos um código via SMS para o seu telefone. <br/> Por favor, confirme o código abaixo:</p>
-                        <div className='flex items-center gap-4'>
-                        <div className={`p-0.5 rounded-3xl bg-gradient-to-b from-[#7F04DF] to-[#313F96]`}>
-                            <input onFocus={e=> {onFocusInput()}}  ref={inputRef1} value={code[0] ? code[0] : ''}  onChange={(e) => {codeOnChange(e.target.value, inputRef2, 0)}} className='h-24 text-lg w-20 font-nonito placeholder-[#CBCBCB] rounded-3xl text-center bg-[#F8F8F8]' type="text" />
+                        <p className='text-[3.70vh] font-nonito'>ESQUECEU A SENHA?</p>
+                        <p className='text-[1.85vh] text-center'>Enviamos um código via SMS para o seu telefone. <br/> Por favor, confirme o código abaixo:</p>
+                        <div className='flex items-center gap-[1.48vh]'>
+                        <div className={`p-[0.18vh] rounded-[2.22vh] bg-gradient-to-b from-[#7F04DF] to-[#313F96]`}>
+                            <input onFocus={e=> {onFocusInput()}}  ref={inputRef1} value={code[0] ? code[0] : ''}  onChange={(e) => {codeOnChange(e.target.value, inputRef2, 0)}} className='h-[8.88vh] text-[2vh] w-[7.40vh] font-nonito placeholder-[#CBCBCB] rounded-[2.22vh] text-center bg-[#F8F8F8]' type="text" />
                         </div> 
-                        <div className={`p-0.5 rounded-3xl bg-gradient-to-b from-[#7F04DF] to-[#313F96]`}>
-                            <input onFocus={e=> {onFocusInput()}} ref={inputRef2} value={code[1] ? code[1] : ''} onChange={(e) => {codeOnChange(e.target.value, inputRef3, 1)}} className='h-24 text-lg w-20 font-nonito placeholder-[#CBCBCB] rounded-3xl text-center bg-[#F8F8F8]' type="text" />
+                        <div className={`p-[0.18vh] rounded-[2.22vh] bg-gradient-to-b from-[#7F04DF] to-[#313F96]`}>
+                            <input onFocus={e=> {onFocusInput()}} ref={inputRef2} value={code[1] ? code[1] : ''} onChange={(e) => {codeOnChange(e.target.value, inputRef3, 1)}} className='h-[8.88vh] text-[2vh] w-[7.40vh] font-nonito placeholder-[#CBCBCB] rounded-[2.22vh] text-center bg-[#F8F8F8]' type="text" />
                         </div>  
-                        <div className={`p-0.5 rounded-3xl bg-gradient-to-b from-[#7F04DF] to-[#313F96]`}>
-                            <input onFocus={e=> {onFocusInput()}} ref={inputRef3} value={code[2] ? code[2] : ''} onChange={(e) => {codeOnChange(e.target.value, inputRef4, 2)}} className='h-24 text-lg w-20 font-nonito placeholder-[#CBCBCB] rounded-3xl text-center bg-[#F8F8F8]' type="text" />
+                        <div className={`p-[0.18vh] rounded-[2.22vh] bg-gradient-to-b from-[#7F04DF] to-[#313F96]`}>
+                            <input onFocus={e=> {onFocusInput()}} ref={inputRef3} value={code[2] ? code[2] : ''} onChange={(e) => {codeOnChange(e.target.value, inputRef4, 2)}} className='h-[8.88vh] text-[2vh] w-[7.40vh] font-nonito placeholder-[#CBCBCB] rounded-[2.22vh] text-center bg-[#F8F8F8]' type="text" />
                         </div>  
-                        <div className={`p-0.5 rounded-3xl bg-gradient-to-b from-[#7F04DF] to-[#313F96]`}>
-                            <input onFocus={e=> {onFocusInput()}} ref={inputRef4} value={code[3] ? code[3] : ''} onChange={(e) => {codeOnChange(e.target.value, false, 3)}}  className='h-24 text-lg w-20 font-nonito placeholder-[#CBCBCB] rounded-3xl text-center bg-[#F8F8F8]' type="text" />
+                        <div className={`p-[0.18vh] rounded-[2.22vh] bg-gradient-to-b from-[#7F04DF] to-[#313F96]`}>
+                            <input onFocus={e=> {onFocusInput()}} ref={inputRef4} value={code[3] ? code[3] : ''} onChange={(e) => {codeOnChange(e.target.value, false, 3)}}  className='h-[8.88vh] text-[2vh] w-[7.40vh] font-nonito placeholder-[#CBCBCB] rounded-[2.22vh] text-center bg-[#F8F8F8]' type="text" />
                         </div>                              
                         </div>
-                        <div className='flex gap-5'>
+                        <div className='flex gap-[1.85vh]'>
                             <ButtonForm className={'bg-[#7A08DB] text-white'} onClick={e=>submitInput()} text={'Confirmar Código'}/>
                             <ButtonForm className={'bg-white border-[#7A08DB]  border-2 text-[#7A08DB]'} onClick={e=>submitInput()} text={'Reenviar Código'}/>
                         </div>
@@ -115,6 +115,7 @@ export default function Register() {
   return (
     <>      
         <div className="h-screen w-screen flex items-center justify-center px-40 py-20 rounded bg-gradient-to-b from-[#7F04DF] to-[#313F96] ">
+            
             <div className='w-1/2 py-16 bg-white rounded-2xl'>
                 
                     {switchModal()}
